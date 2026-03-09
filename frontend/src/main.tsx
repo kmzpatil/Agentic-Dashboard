5edabd868a96659e518eca1bdf086c950f586f83
Naitik Agarwal <naitikagar12@gmail.com>
2026-03-09 23:18:45 +0530

    fix code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import VideoExplorerTable from './components/VideoExplorerTable.tsx'
+// import DashboardLayout from './App2.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* <Route path="/chatbot" element={<DashboardLayout />} /> */}
+      </Routes>
+    </BrowserRouter>
+    {/* <VideoExplorerTable/> */}
   </StrictMode>,
 )
